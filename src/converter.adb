e009424327c520fb5e4759a6cb2ae36f2b9be8f0
M. Gardner <magardner2010@gmail.com>
2020-05-30 14:20:22 +0300

    italicise

--- src/converter.adb
+++ src/converter.adb
@@ -7,14 +7,29 @@ package body converter is
       for I in S'Range loop
          if I /= S'Last and S(I) = '*' then
             if S(I+1) = '*' then
-            return S(S'First .. I-1) &
-            (if found then "</b>" else "<b>") &
-              boldify(S(I+2 .. S'Last),not found);
+               return S(S'First .. I-1) &
+               (if found then "</b>" else "<b>") &
+                 boldify(S(I+2 .. S'Last),not found);
             end if;
-            end if;
+         end if;
       end loop;
       return S;
    end boldify;
+   -- finds things wrapped in **-s and turns them into <b></b>
+   function italicise(S: String;found:Boolean:=False) return String is
+   begin
+      if S="" then
+         return "";
+      end if;
+      for I in S'Range loop
+         if S(I) = '*' then
+            return S(S'First .. I-1) &
+            (if found then "</b>" else "<b>") &
+              italicise(S(I+2 .. S'Last),not found);
+         end if;
+      end loop;
+      return S;
+   end italicise;
    -- finds things wrapped in **-s and turns them into <b></b>
 
 
--- src/converter.ads
+++ src/converter.ads
@@ -1,4 +1,4 @@
 package converter is
    function boldify(S: String;found:Boolean:=False) return String;
-
+   function italicise(S: String;found:Boolean:=False) return String;
 end converter;

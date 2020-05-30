bb685127f27236015d574a0cfd5cbd0644a08d1e
M. Gardner <magardner2010@gmail.com>
2020-05-30 11:58:34 +0300

    autocommit

--- src/converter.adb
+++ src/converter.adb
@@ -1,10 +1,11 @@
 package converter is
    function boldify(S: String) return String is
-      I:integer
+      I:integer;
    begin
-         if S="" then
-            return S;
-         end if;
+      if S="" then
+         return S;
+      end if;
+      for I in S'Range loop
 
    end boldify;
    -- finds things wrapped in *-s and turns them into <b></b>

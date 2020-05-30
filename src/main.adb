15045261d6b232b900a9c3e5eded2e626964d7c3
M. Gardner <magardner2010@gmail.com>
2020-05-30 18:18:04 +0300

    hopefully, this will work as a rudimentary sub-server

--- src/main.adb
+++ src/main.adb
@@ -8,7 +8,33 @@ use GNAT.Sockets;
 with converter;
 
 procedure Main is
-   S :String := Get_Line;
+	function request return String is
+		Address : Sock_addr_type;
+		server:  Socket_type;
+		socket : Socket_type;
+		Channel: Stream_access;
+	begin
+		Address.Addr := Addresses (Get_Host_By_Name (Host_Name), 1);
+		Address.Port := 5432;
+		Create_socket(server);
+		Set_socket_option(server,socket_level,(reuse_address,true));
+		bind_socket(server,Address);
+		listen_socket(server);
+		Accept_Socket (Server, Socket, Address);
+		Channel := Stream (Socket);
+		delay 0.2;
+		declare
+			Message : string :=converter.linkify(converter.italicise(converter.boldify(String'input(channel))));
+		begin
+			string'output(channel,Message);
+			return Message;
+		end;
+	end request;
 begin
-   Put_Line(converter.linkify(converter.italicise(converter.boldify(S))));
+
+	declare
+		S : String := Get_Line;
+	begin
+		Put_Line(converter.linkify(converter.italicise(converter.boldify(S))));
+	end;
 end Main;

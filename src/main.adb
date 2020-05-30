38ab5f5e5a45059465e49c97317d1a54e58ffd90
M. Gardner <magardner2010@gmail.com>
2020-05-30 12:15:41 +0300

    autocommit

--- src/main.adb
+++ src/main.adb
@@ -7,12 +7,5 @@ with converter;
 procedure Main is
    S :String := Get_Line;
 begin
-   --  Insert code here.
-   -- while echo = 0 loop
-   --  null;
-   -- end loop;
    Put_Line(converter.boldify(S));
-   for I in S'First .. S'Last loop
-      Put_Line(S(I)'Image);
-   end loop;
 end Main;

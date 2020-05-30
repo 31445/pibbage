224003589493425e85c652181fea83463cda9ca5
M. Gardner <magardner2010@gmail.com>
2020-05-30 12:12:32 +0300

    autocommit

--- src/main.adb
+++ src/main.adb
@@ -8,9 +8,10 @@ procedure Main is
    S :String := Get_Line;
 begin
    --  Insert code here.
-  -- while echo = 0 loop
-    --  null;
-  -- end loop;
+   -- while echo = 0 loop
+   --  null;
+   -- end loop;
+   Put_Line(converter.boldify(S));
    for I in S'First .. S'Last loop
       Put_Line(S(I)'Image);
    end loop;

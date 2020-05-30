269f851452b8c1bad5bf555db20ea39488f0ca90
M. Gardner <magardner2010@gmail.com>
2020-05-30 14:51:35 +0300

    add links (to be tested)

--- src/converter.adb
+++ src/converter.adb
@@ -31,6 +31,35 @@ package body converter is
       return S;
    end italicise;
    -- finds things wrapped in **-s and turns them into <b></b>
-
+   function linkify(S: String) return string is
+	   link_start: Integer :=-1;
+	   link_end: Integer :=-1;
+	   href_start: Integer :=-1;
+	   href_end: Integer := -1;
+   begin
+	if S = "" then
+		return "";
+	end if;
+	for I in S'Range loop
+		if href_end = -1 then
+			return S( S'First .. link_start-1) &
+				"<a href='" & S(href_start+1 .. href_end-1) & "' >" &
+				S(link_start+1 ..link_end-1) & "</a>"&
+				linkify(S( href_end+1 .. S'Last));
+		end if;
+		if S(I) = '[' then
+			link_start:=I;
+		end if;
+		if link_start /= -1 and S(I) = ']' then
+			link_end:=I;
+		end if;
+		if link_end /= -1 and link_end = I-1 and S(I) = '(' then
+			href_start:=I;
+		end if;
+		if href_start /= -1 and S(I) = ')' then
+			href_end:=I;
+		end if;
+	end loop;
+   end linkify;
 
 end converter;
--- src/converter.ads
+++ src/converter.ads
@@ -1,4 +1,5 @@
 package converter is
    function boldify(S: String;found:Boolean:=False) return String;
    function italicise(S: String;found:Boolean:=False) return String;
+   function linkify(S: String) return String;
 end converter;

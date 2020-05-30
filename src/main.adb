0b503bfe4c727b96b1cbdfb3fcdc00fad5a98673
M. Gardner <magardner2010@gmail.com>
2020-05-30 11:28:13 +0300

    autocommit

--- src/main.adb
+++ src/main.adb
@@ -1,15 +1,15 @@
 with Ada.Text_IO;
 use Ada.Text_IO;
 
-with echo;
-
+--with echo;
 
 procedure Main is
+   S :String := Get_Line;
 begin
    --  Insert code here.
-   while echo = 0 loop
-      null;
-   end loop;
+  -- while echo = 0 loop
+    --  null;
+  -- end loop;
 
    null;
 end Main;

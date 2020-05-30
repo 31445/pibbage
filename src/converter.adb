8f6c46e42683195b2c3b08e361c01929b650301c
M. Gardner <magardner2010@gmail.com>
2020-05-30 14:57:31 +0300

    debuging info. to be deleted

--- src/converter.adb
+++ src/converter.adb
@@ -1,3 +1,6 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
 package body converter is
    function boldify(S: String;found:Boolean:=False) return String is
    begin
@@ -49,15 +52,19 @@ package body converter is
 		end if;
 		if S(I) = '[' then
 			link_start:=I;
+			Put_Line(link_start'Image & ' ' & link_end'Image);
 		end if;
 		if link_start /= -1 and S(I) = ']' then
 			link_end:=I;
+			Put_Line(link_start'Image & ' ' & link_end'Image);
 		end if;
 		if link_end /= -1 and link_end = I-1 and S(I) = '(' then
 			href_start:=I;
+			Put_Line(href_start'Image & ' ' & href_end'Image);
 		end if;
 		if href_start /= -1 and S(I) = ')' then
 			href_end:=I;
+			Put_Line(href_start'Image & ' ' & href_end'Image);
 		end if;
 	end loop;
 	return S;

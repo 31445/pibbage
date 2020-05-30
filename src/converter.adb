ea46cf16b1e472cf4c890b4c726e1dc4b1918300
M. Gardner <magardner2010@gmail.com>
2020-05-30 14:08:37 +0300

    make bold not italic

--- src/converter.adb
+++ src/converter.adb
@@ -2,19 +2,18 @@ package body converter is
    function boldify(S: String;found:Boolean:=False) return String is
    begin
       if S="" then
-         return S;
+         return "";
       end if;
       for I in S'Range loop
-         if S(I)='*' then
-            --TODO
+         if S(I .. I+1)="**" then
             return S(S'First .. I-1) &
             (if found then "</b>" else "<b>") &
-              boldify(S(I+1 .. S'Last),not found);
+              boldify(S(I+2 .. S'Last),not found);
          end if;
       end loop;
       return S;
    end boldify;
-   -- finds things wrapped in *-s and turns them into <b></b>
+   -- finds things wrapped in **-s and turns them into <b></b>
 
 
 end converter;

1f7915fb81f5f085e1c359a0c0eca7c98752532a
M. Gardner <magardner2010@gmail.com>
2020-05-30 11:13:31 +0300

    add echo function

---
+++ src/echo.adb
@@ -0,0 +1,12 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+function echo return Integer is
+   S : String := Get_Line;
+begin
+   Put_Line(S);
+   if S = "stop" or S = "stop " then
+      return 1;
+   else
+      return 0;
+   end if
+end echo
